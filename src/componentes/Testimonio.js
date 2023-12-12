5a066bdce243f1575b16dd106d15307272866d2c
Andres Uribe <andres.uribe.gutierrez@gmail.com>
2023-12-11 23:03:15 -0500

    Se agregan imagenes y se prepara el proyecto

--- src/componentes/Testimonio.js
+++ src/componentes/Testimonio.js
@@ -2,6 +2,20 @@ import React from "react";
 
 function Testimonio (){
   return(
-x
+    <div className="contenedor-testimonio">
+      <img
+        className="imagen-testimonio"
+        src={require("../imagenes/testimonio-mujer1.jpeg")}
+        alt="foto de Emma"/>
+   
+      <div className="contenedor-texto-testimonio">
+        <p className="nombre-testimonio">Emma Bostian en SUecia</p>
+        <p className="cargo-testimonio">Ingeniera de Software en Spotify</p>
+        <p className="texto-testimonio">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+        
+
+    </div>
+    </div>
   );          
 }
+export default Testimonio;
